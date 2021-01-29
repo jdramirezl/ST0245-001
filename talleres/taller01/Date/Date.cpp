31def75a6ab1b77f8da1c08c3dad97db633dd03d
Julian David Ramirez Lopera <jdramirezl@eafit.edu.co>
2021-01-29 11:41:45 -0500

    changes

--- talleres/taller01/Date/Date.cpp
+++ talleres/taller01/Date/Date.cpp
@@ -2,52 +2,51 @@
 
 using namespace std;
 
-class Date
-{
+class Date{
     int day;
     int month;
     int year;
 
-public:
-    Date(int day, int month, int year){
-        this->day = day;
-        this->month = month;
-        this->year = year;
-    }
-
-    int get_day(){
-        return this->day;
-    }
-
-    int get_month(){
-        return this->month;
-    }
-
-    int get_year(){
-        return this->year;
-    }
-
-    int compare_dates(Date *other){
-        if (this->year < other->year or this->year == other->year and this->month < other->month or this->year == other->year and this->month == other->month and this->day < other->day){
-            cout << "-1" << endl;
+    public:
+        Date(int day, int month, int year){
+            this->day = day;
+            this->month = month;
+            this->year = year;
         }
-        else if (this->year > other->year or this->year == other->year and this->month > other->month or this->year == other->year and this->month == other->month and this->day > other->day){
-            cout << "0" << endl;
+
+        int get_day(){
+            return this->day;
         }
-        else{
-            cout << "1" << endl;
+
+        int get_month(){
+            return this->month;
         }
+
+        int get_year(){
+            return this->year;
+        }
+
+        int compare_dates(Date *other){
+            if (this->year < other->year or this->year == other->year and this->month < other->month or this->year == other->year and this->month == other->month and this->day < other->day){
+                return -1;
+            }
+            else if (this->year > other->year or this->year == other->year and this->month > other->month or this->year == other->year and this->month == other->month and this->day > other->day){
+                return 0;
+            }
+            else{
+                return 1;
+            }
+        }
+    };
+
+    std::ostream &operator<<(std::ostream &out, Date &date){
+        return out << "Date: " + to_string(date.get_day()) + "/" + to_string(date.get_month()) + "/" + to_string(date.get_year()) << endl;
     }
-};
 
-std::ostream &operator<<(std::ostream &out, Date &date){
-    return out << "Date: " + to_string(date.get_day()) + "/" + to_string(date.get_month()) + "/" + to_string(date.get_year()) << endl;
-}
+    int main(){
+        Date hoy(27, 1, 2021), ayer(26, 1, 2021);
 
-int main(){
-    Date hoy(27, 1, 2021), ayer(26, 1, 2021);
-
-    cout << hoy.get_day() << endl;
-    hoy.compare_dates(&ayer);
-    cout << hoy << endl;
+        cout << hoy.get_day() << endl;
+        hoy.compare_dates(&ayer);
+        cout << hoy << endl;
 }
