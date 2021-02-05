2c8355ffc0ff5fcc9bb4d4a14788afbb7ca4d6f9
Julian <jdramirezl@eafit.edu.co>
2021-02-04 21:25:09 -0500

    changes

---
+++ talleres/taller06/patrones.py
@@ -0,0 +1,6 @@
+
+n = int(input())
+
+result = [[x for x in range(1, i+1)] for i in range(1, n+1)]
+
+print(result)
---
+++ talleres/taller06/reverse.py
@@ -0,0 +1,12 @@
+
+result = []
+
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    result.append(n)
+
+print(list(reversed(result)))
+
+
